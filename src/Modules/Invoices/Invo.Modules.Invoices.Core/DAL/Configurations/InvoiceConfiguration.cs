c287bd51bc1e001c986fb0172fdc58ecfb19dbed
gixerr <pwardyn213@gmail.com>
2022-02-19 21:49:28 +0100

    added ef core configurations

---
+++ src/Modules/Invoices/Invo.Modules.Invoices.Core/DAL/Configurations/InvoiceConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Invo.Modules.Invoices.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Invo.Modules.Invoices.Core.DAL.Configurations
+{
+    internal class InvoiceConfiguration : IEntityTypeConfiguration<Invoice>
+    {
+        public void Configure(EntityTypeBuilder<Invoice> builder)
+        {
+            
+        }
+    }
+}
---
+++ src/Modules/Invoices/Invo.Modules.Invoices.Core/DAL/Configurations/InvoiceItemConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Invo.Modules.Invoices.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Invo.Modules.Invoices.Core.DAL.Configurations
+{
+    internal class InvoiceItemConfiguration : IEntityTypeConfiguration<InvoiceItem>
+    {
+        public void Configure(EntityTypeBuilder<InvoiceItem> builder)
+        {
+        }
+    }
+}

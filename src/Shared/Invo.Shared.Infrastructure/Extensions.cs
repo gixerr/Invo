516b75c55b6d51b2ab0a102adef673688dc38ea9
gixerr <pwardyn213@gmail.com>
2022-02-19 21:50:48 +0100

    added options

--- src/Shared/Invo.Shared.Infrastructure/Extensions.cs
+++ src/Shared/Invo.Shared.Infrastructure/Extensions.cs
@@ -5,6 +5,7 @@ using Invo.Shared.Infrastructure.Exceptions;
 using Invo.Shared.Infrastructure.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 [assembly: InternalsVisibleTo("Invo.Bootstrapper")]
@@ -38,6 +39,20 @@ namespace Invo.Shared.Infrastructure
             });
             
             return app;
+        }
+
+        public static T GetOptions<T>(this IServiceCollection services, string sectionName) where T : new()
+        {
+            using var serviceProvider = services.BuildServiceProvider();
+            var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+            return configuration.GetOptions<T>(sectionName);
+        }
+
+        public static T GetOptions<T>(this IConfiguration configuration, string sectionName) where T : new()
+        {
+            var options = new T();
+            configuration.GetSection(sectionName).Bind(options);
+            return options;
         }
     }
 }
---
+++ src/Shared/Invo.Shared.Infrastructure/Postgres/PostgresOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace Invo.Shared.Infrastructure.Postgres
+{
+    public class PostgresOptions
+    {
+        public string ConnectionString { get; set; }
+    }
+}

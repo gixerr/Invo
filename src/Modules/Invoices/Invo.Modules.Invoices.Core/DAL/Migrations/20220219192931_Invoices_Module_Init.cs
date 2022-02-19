58aec2fc5c6ca1ed3a572d42c8cc3004c30b1064
gixerr <pwardyn213@gmail.com>
2022-02-19 21:53:10 +0100

    added ef core migration

---
+++ src/Modules/Invoices/Invo.Modules.Invoices.Core/DAL/Migrations/20220219192931_Invoices_Module_Init.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Invo.Modules.Invoices.Core.DAL.Migrations
+{
+    public partial class Invoices_Module_Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "invoices");
+
+            migrationBuilder.CreateTable(
+                name: "Invoices",
+                schema: "invoices",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: true),
+                    Number = table.Column<string>(type: "text", nullable: true),
+                    DateOfIssue = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    SaleDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    SellerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    BuyerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    VatAmount = table.Column<decimal>(type: "numeric", nullable: false),
+                    NetAmount = table.Column<decimal>(type: "numeric", nullable: false),
+                    GrossAmount = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Invoices", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InvoiceItems",
+                schema: "invoices",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    InvoiceId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Unit = table.Column<string>(type: "text", nullable: true),
+                    NetPrice = table.Column<decimal>(type: "numeric", nullable: false),
+                    GrossPrice = table.Column<decimal>(type: "numeric", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    NetAmount = table.Column<decimal>(type: "numeric", nullable: false),
+                    VatRate = table.Column<int>(type: "integer", nullable: false),
+                    VatAmount = table.Column<decimal>(type: "numeric", nullable: false),
+                    GrossAmount = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InvoiceItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_InvoiceItems_Invoices_InvoiceId",
+                        column: x => x.InvoiceId,
+                        principalSchema: "invoices",
+                        principalTable: "Invoices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InvoiceItems_InvoiceId",
+                schema: "invoices",
+                table: "InvoiceItems",
+                column: "InvoiceId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InvoiceItems",
+                schema: "invoices");
+
+            migrationBuilder.DropTable(
+                name: "Invoices",
+                schema: "invoices");
+        }
+    }
+}

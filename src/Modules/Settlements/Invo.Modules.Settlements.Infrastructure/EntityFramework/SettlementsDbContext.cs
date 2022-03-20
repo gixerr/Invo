a2cd8c27f08c3e1111dbc53f340cf917866ebf88
gixerr <pwardyn213@gmail.com>
2022-03-20 22:20:38 +0100

    [SETTLEMENTS] added dbContext

---
+++ src/Modules/Settlements/Invo.Modules.Settlements.Infrastructure/EntityFramework/SettlementsDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Invo.Modules.Settlements.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Invo.Modules.Settlements.Infrastructure.EntityFramework
+{
+    internal class SettlementsDbContext : DbContext
+    {
+        public DbSet<IncomeInvoice> IncomeInvoices { get; set; }
+        public DbSet<CostInvoice> CostInvoices { get; set; }
+
+        public SettlementsDbContext(DbContextOptions<SettlementsDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("settlements");
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        }
+    }
+}

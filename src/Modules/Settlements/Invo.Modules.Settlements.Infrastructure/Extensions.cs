c3aa3868c38406685fb805ebde298d56dd6bd88f
gixerr <pwardyn213@gmail.com>
2022-03-20 22:21:53 +0100

    [SETTLEMENTS] updated extensions

--- src/Modules/Settlements/Invo.Modules.Settlements.Infrastructure/Extensions.cs
+++ src/Modules/Settlements/Invo.Modules.Settlements.Infrastructure/Extensions.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Invo.Modules.Settlements.Domain.Repositories;
+using Invo.Modules.Settlements.Infrastructure.EntityFramework;
+using Invo.Modules.Settlements.Infrastructure.EntityFramework.Repositories;
+using Invo.Shared.Infrastructure.Postgres;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Invo.Modules.Settlements.Infrastructure
@@ -6,6 +9,9 @@ namespace Invo.Modules.Settlements.Infrastructure
     public static class Extensions
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
-            => services;
+            => services
+                .AddPostgres<SettlementsDbContext>()
+                .AddScoped<IIncomeInvoiceRepository, PostgresIncomeInvoiceRepository>()
+                .AddScoped<ICostInvoiceRepository, PostgresCostInvoiceRepository>();
     }
 }

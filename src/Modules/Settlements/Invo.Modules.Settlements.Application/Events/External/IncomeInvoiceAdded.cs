df7c3b7efc05df3bca8dfc30fcf8d222518935ea
gixerr <pwardyn213@gmail.com>
2022-03-23 22:05:55 +0100

    [SETTLEMENTS] added IncomeInvoiceAdded event

---
+++ src/Modules/Settlements/Invo.Modules.Settlements.Application/Events/External/IncomeInvoiceAdded.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Invo.Shared.Abstractions.Events;
+
+namespace Invo.Modules.Settlements.Application.Events.External
+{
+    public record IncomeInvoiceAdded(Guid Id, string Type, string Number, DateTime DateOfIssue, decimal VatAmount, decimal NetAmount,
+        Guid SellerId) : IEvent
+    {
+    }
+}

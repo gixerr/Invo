3a5d2860118d0a037a300d761478b11d61ba8315
gixerr <pwardyn213@gmail.com>
2022-03-23 22:07:25 +0100

    [SETTLEMENTS] added InvoiceAlreadyExistsException

---
+++ src/Modules/Settlements/Invo.Modules.Settlements.Application/Exceptions/InvoiceAlreadyExistsException.cs
@@ -0,0 +1,11 @@
+﻿using Invo.Shared.Abstractions.Exceptions;
+
+namespace Invo.Modules.Settlements.Application.Exceptions
+{
+    public class InvoiceAlreadyExistsException : InvoException
+    {
+        public InvoiceAlreadyExistsException(string invoiceNumber) : base($"Invoice with number '{invoiceNumber}' was already added")
+        {
+        }
+    }
+}

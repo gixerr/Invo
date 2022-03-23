8fec91373bdce93517e7c12c9735024323753839
gixerr <pwardyn213@gmail.com>
2022-03-23 22:05:30 +0100

    [INVOICES] added IncomeInvoiceAdded event

---
+++ src/Modules/Invoices/Invo.Modules.Invoices.Core/Events/IncomeInvoiceAdded.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using Invo.Shared.Abstractions.Events;
+
+namespace Invo.Modules.Invoices.Core.Events
+{
+    public record IncomeInvoiceAdded(Guid Id, string Type, string Number, DateTime DateOfIssue,
+        decimal VatAmount, decimal NetAmount, Guid SellerId) : IEvent;
+}

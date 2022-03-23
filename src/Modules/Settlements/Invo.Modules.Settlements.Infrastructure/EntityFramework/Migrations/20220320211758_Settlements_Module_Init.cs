fe8fc0e58a324c6875355dca50550b41590773e5
gixerr <pwardyn213@gmail.com>
2022-03-23 22:17:26 +0100

    [EF] added migrations

---
+++ src/Modules/Settlements/Invo.Modules.Settlements.Infrastructure/EntityFramework/Migrations/20220320211758_Settlements_Module_Init.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Invo.Modules.Settlements.Infrastructure.EntityFramework.Migrations
+{
+    public partial class Settlements_Module_Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "settlements");
+
+            migrationBuilder.CreateTable(
+                name: "CostInvoices",
+                schema: "settlements",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    BuyerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IsCarInvoice = table.Column<bool>(type: "boolean", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: true),
+                    DateOfIssue = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    VatAmount = table.Column<decimal>(type: "numeric", nullable: false),
+                    NetAmount = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CostInvoices", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IncomeInvoices",
+                schema: "settlements",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SellerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: true),
+                    DateOfIssue = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    VatAmount = table.Column<decimal>(type: "numeric", nullable: false),
+                    NetAmount = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IncomeInvoices", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CostInvoices",
+                schema: "settlements");
+
+            migrationBuilder.DropTable(
+                name: "IncomeInvoices",
+                schema: "settlements");
+        }
+    }
+}
---
+++ src/Modules/Settlements/Invo.Modules.Settlements.Infrastructure/EntityFramework/Migrations/20220323191522_Settlement_Module_Invoices_Alter.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Invo.Modules.Settlements.Infrastructure.EntityFramework.Migrations
+{
+    public partial class Settlement_Module_Invoices_Alter : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Number",
+                schema: "settlements",
+                table: "IncomeInvoices",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Number",
+                schema: "settlements",
+                table: "CostInvoices",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Number",
+                schema: "settlements",
+                table: "IncomeInvoices");
+
+            migrationBuilder.DropColumn(
+                name: "Number",
+                schema: "settlements",
+                table: "CostInvoices");
+        }
+    }
+}

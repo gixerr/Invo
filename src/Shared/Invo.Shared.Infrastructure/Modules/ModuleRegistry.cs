b18a36d3e959a20b015ac71635f7e6be514018b7
gixerr <pwardyn213@gmail.com>
2022-03-23 22:18:22 +0100

    [SOLUTION] code cleanup

--- src/Shared/Invo.Shared.Infrastructure/Modules/ModuleRegistry.cs
+++ src/Shared/Invo.Shared.Infrastructure/Modules/ModuleRegistry.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Invo.Shared.Abstractions.Modules;
 
 namespace Invo.Shared.Infrastructure.Modules
 {

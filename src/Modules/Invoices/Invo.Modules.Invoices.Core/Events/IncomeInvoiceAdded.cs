94e56715b8d5e0c88bcfa048941c1603927816e5
gixerr <pwardyn213@gmail.com>
2022-03-27 12:34:02 +0200

    [INVOICES] set event as internal

--- src/Modules/Invoices/Invo.Modules.Invoices.Core/Events/IncomeInvoiceAdded.cs
+++ src/Modules/Invoices/Invo.Modules.Invoices.Core/Events/IncomeInvoiceAdded.cs
@@ -3,6 +3,6 @@ using Invo.Shared.Abstractions.Events;
 
 namespace Invo.Modules.Invoices.Core.Events
 {
-    public record IncomeInvoiceAdded(Guid Id, string Type, string Number, DateTime DateOfIssue,
-        decimal VatAmount, decimal NetAmount, Guid SellerId) : IEvent;
+    internal record IncomeInvoiceAdded(Guid Id, string Type, string Number, DateTime DateOfIssue, decimal VatAmount,
+        decimal NetAmount, Guid SellerId) : IEvent;
 }

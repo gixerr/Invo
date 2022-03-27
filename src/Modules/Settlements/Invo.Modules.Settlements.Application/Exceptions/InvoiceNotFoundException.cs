2eacc6c31641eb779a5a88034f4f336826d3f609
gixerr <pwardyn213@gmail.com>
2022-03-27 20:35:14 +0200

    [SETTLEMENTS] added InvoiceNotFoundException

---
+++ src/Modules/Settlements/Invo.Modules.Settlements.Application/Exceptions/InvoiceNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Invo.Shared.Abstractions.Exceptions;
+
+namespace Invo.Modules.Settlements.Application.Exceptions
+{
+    public class InvoiceNotFoundException : InvoException
+    {
+        public InvoiceNotFoundException(Guid id) : base($"Invoice with ID: '{id}' was not found")
+        {
+        }
+    }
+}

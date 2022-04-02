0dc088a45effb7615e8643f7dea3eaec2736757d
gixerr <pwardyn213@gmail.com>
2022-04-02 14:18:28 +0200

    [SETTLEMENTS] added SettlementsController

---
+++ src/Modules/Settlements/Invo.Modules.Settlements.Api/Controllers/SettlementsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Invo.Modules.Settlements.Application.Commands;
+using Invo.Shared.Abstractions.Commands;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Invo.Modules.Settlements.Api.Controllers
+{
+    internal class SettlementsController : BaseController
+    {
+        private readonly ICommandDispatcher _commandDispatcher;
+
+        public SettlementsController(ICommandDispatcher commandDispatcher)
+        {
+            _commandDispatcher = commandDispatcher;
+        }
+        
+        [HttpPost("month/create")]
+        public async Task<ActionResult> CreateMonthSettlement(CreateMonthSettlement command)
+        {
+            await _commandDispatcher.SendAsync(command);
+            return Ok();
+        }
+    }
+}

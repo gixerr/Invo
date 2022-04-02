107dcc6c50174021f1500069de71f658e9816e89
gixerr <pwardyn213@gmail.com>
2022-04-02 14:12:36 +0200

    [SETTLEMENTS] added value objects

---
+++ src/Modules/Settlements/Invo.Modules.Settlements.Domain/ValueObjects/MixedCycleCarCost.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Invo.Shared.Abstractions.Calculations;
+
+namespace Invo.Modules.Settlements.Domain.ValueObjects
+{
+    public class MixedCycleCarCost : ICost
+    {
+        public decimal CostAmount { get; }
+        public decimal VatAmount { get; set; }
+
+        public MixedCycleCarCost(decimal taxAmount, decimal vatAmount)
+        {
+            VatAmount = Math.Round(vatAmount * (decimal)0.5, 2);
+            CostAmount =  Math.Round(taxAmount * (decimal)0.75, 2);
+        }
+    }
+}
---
+++ src/Modules/Settlements/Invo.Modules.Settlements.Domain/ValueObjects/UnboundedCost.cs
@@ -0,0 +1,16 @@
+﻿using Invo.Shared.Abstractions.Calculations;
+
+namespace Invo.Modules.Settlements.Domain.ValueObjects
+{
+    public class UnboundedCost : ICost
+    {
+        public decimal CostAmount { get; }
+        public decimal VatAmount { get; set; }
+
+        public UnboundedCost(decimal taxAmount, decimal vatAmount)
+        {
+            CostAmount = taxAmount;
+            VatAmount = vatAmount;
+        }
+    }
+}

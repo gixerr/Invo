f4f99d704813498929b36faa67c5c13bbb7de417
gixerr <pwardyn213@gmail.com>
2022-04-07 22:39:24 +0200

    [SETTLEMENTS] updated CostCalculationService to use models

--- src/Modules/Settlements/Invo.Modules.Settlements.Application/Services/CostCalculationService.cs
+++ src/Modules/Settlements/Invo.Modules.Settlements.Application/Services/CostCalculationService.cs
@@ -2,19 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Invo.Modules.Settlements.Domain.Entities;
-using Invo.Modules.Settlements.Domain.ValueObjects.Costs;
+using Invo.Modules.Settlements.Domain.Models.Costs;
+using Invo.Modules.Settlements.Domain.Models.Payments;
 using Invo.Shared.Abstractions.Calculations;
 
 namespace Invo.Modules.Settlements.Application.Services
 {
     internal sealed class CostCalculationService : ICostCalculationService
     {
-        public IEnumerable<ICost> ProcessCosts(IEnumerable<CostInvoice> costs)
+        public IEnumerable<ICost> CalculateCostsIncludingLimitations(IEnumerable<CostInvoice> costs)
             => costs.Select(cost => (ICost)(cost.IsCarInvoice
                 ? new MixedCycleCarCost(cost.NetAmount, cost.VatAmount)
                 : new UnboundedCost(cost.NetAmount, cost.VatAmount)));
 
-        public decimal GetToSpentValue(decimal totalMonthIncome, decimal totalIncomeVat, decimal taxToPay, decimal vatToPay)
-            => Math.Round(totalMonthIncome + totalIncomeVat - taxToPay - vatToPay, 2);
+        public decimal GetToSpentValue(MonthIncome monthIncome, MonthIncomeVat monthIncomeVat, TaxToPay taxToPay,
+            VatToPay vatToPay)
+            => Math.Round(monthIncome.Value + monthIncomeVat.Value - taxToPay.Value - vatToPay.Value, 2);
     }
 }
--- src/Modules/Settlements/Invo.Modules.Settlements.Application/Services/ICostCalculationService.cs
+++ src/Modules/Settlements/Invo.Modules.Settlements.Application/Services/ICostCalculationService.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections.Generic;
 using Invo.Modules.Settlements.Domain.Entities;
+using Invo.Modules.Settlements.Domain.Models.Payments;
 using Invo.Shared.Abstractions.Calculations;
 
 namespace Invo.Modules.Settlements.Application.Services
 {
     public interface ICostCalculationService
     {
-        IEnumerable<ICost> ProcessCosts(IEnumerable<CostInvoice> costs);
-        decimal GetToSpentValue(decimal totalMonthIncome, decimal totalIncomeVat, decimal taxToPay, decimal vatToPay);
+        IEnumerable<ICost> CalculateCostsIncludingLimitations(IEnumerable<CostInvoice> costs);
+        decimal GetToSpentValue(MonthIncome monthIncome, MonthIncomeVat monthIncomeVat, TaxToPay taxToPay, VatToPay vatToPay);
     }
 }

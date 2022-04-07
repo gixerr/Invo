65bac7877d26f846623a3c26591355e90057843c
gixerr <pwardyn213@gmail.com>
2022-04-07 22:36:17 +0200

    [SETTLEMENTS] removed unnecessary service

--- src/Modules/Settlements/Invo.Modules.Settlements.Application/Services/ITaxCalculationService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Invo.Modules.Settlements.Application.Services
-{
-    public interface ITaxCalculationService
-    {
-        decimal TaxRate { get; }
-        decimal GetTaxToPay(decimal income, decimal costs);
-        decimal GetVatToPay(decimal incomeVat, decimal costVat);
-    }
-}
